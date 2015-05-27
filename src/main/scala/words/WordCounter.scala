2fc2bd6b3930dbf8c0a962e8577c1ebaed288419
dkittle <don@kittle.ca>
2015-05-27 07:52:20 -0400

    refactoring to make method names clearer, added func to meet requirements

--- src/main/scala/words/WordCounter.scala
+++ src/main/scala/words/WordCounter.scala
@@ -1,5 +1,6 @@
 package words
 
+import java.io.FileNotFoundException
 import java.nio.charset.CodingErrorAction
 
 import scala.io.Codec
@@ -9,17 +10,21 @@ import scala.io.Codec
  */
 case class WordCounter(sourceText: String) {
 
-  val listOfCountedWords = countWords()
+  val listOfCountedWords = parseWords()
 
-  def countedWords() = {
+  def wordsAndCounts() = {
     listOfCountedWords
+  }
+
+  def countOfAllWords() = {
+    listOfCountedWords.foldLeft(0)(_ + _._2)
   }
 
   /**
    * Arguably not very efficient if countWords is called more than once
    * @return List of tuples with word and # times it appears in string
    */
-  private def countWords(): List[(String, Int)] = {
+  private def parseWords(): List[(String, Int)] = {
     if (sourceText.isEmpty)
       return List[(String,Int)]()
 
@@ -43,7 +48,18 @@ object WordCounter extends App {
     System.exit(0)
   }
   try {
-    val counter = new WordCounter(scala.io.Source.fromFile("src/test/resources/writing.txt").getLines.mkString)
+    val counter = new WordCounter(scala.io.Source.fromFile(args(0)).getLines.mkString)
+    outputFileStats(counter)
+  } catch {
+    case e: FileNotFoundException => println(s"The file named ${args(0)} was not found.")
   }
-  finally {}
+  
+  def outputFileStats(counter: WordCounter) = {
+    println(s"The file contains ${counter.countOfAllWords()} words")
+    println("The top 10 words by the number of times they occurr are:")
+    val topTen = counter wordsAndCounts() take 10
+    for((word, count) <- topTen) {
+      println(s" '${word}' appears ${count} time${if (count > 1) "s" else ""}")
+    }
+  }
 }
--- src/test/scala/words/WordCounterSpec.scala
+++ src/test/scala/words/WordCounterSpec.scala
@@ -9,21 +9,21 @@ class WordCounterSpec extends FlatSpec with Matchers {
 
   "A Word Counter" should "return None from an empty string" in {
     val counter = new WordCounter("")
-    counter countedWords() should have size 0
+    counter wordsAndCounts() should have size 0
   }
 
   it should "return one word from a one word string" in {
     val counter = new WordCounter("the")
-    counter countedWords() should have size 1
-    counter countedWords() should contain (("the",1))
+    counter wordsAndCounts() should have size 1
+    counter wordsAndCounts() should contain (("the",1))
   }
 
   it should "read words from a file" in {
     val counter = new WordCounter(scala.io.Source.fromFile("src/test/resources/writing.txt").getLines.mkString)
     val text = counter.sourceText
-    counter countedWords() should contain (("the",23))
-    counter countedWords() should contain (("narrative",2))
-    counter countedWords() should contain (("convivial",2))
+    counter wordsAndCounts() should contain (("the",23))
+    counter wordsAndCounts() should contain (("narrative",2))
+    counter wordsAndCounts() should contain (("convivial",2))
   }
 
 }
